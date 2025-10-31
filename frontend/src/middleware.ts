69586b1b872a1e099f935a31daeab6e59b998031
VoltrixTeam <bestrustiktok@gmail.com>
2025-10-31 17:13:03 +0300

    frontend(fix): update access token in chat page

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,24 +1,64 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const publicRoutes = ['/auth']
+const authRoutes = ['/auth']
 
-const CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_URL
+const REFRESH_TOKEN_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
 	const { pathname } = request.nextUrl
 	const refreshToken = request.cookies.get('refreshToken')?.value
-
-	if (!CLIENT_URL) {
-		throw new Error(
-			'Переменная окружения NEXT_PUBLIC_CLIENT_URL не найдена'
-		)
-	}
+	const accessToken = request.cookies.get('accessToken')?.value
 
 	if (refreshToken) {
-		if (publicRoutes.includes(pathname)) {
-			return NextResponse.redirect(new URL(`${CLIENT_URL}`, request.url))
+		if (authRoutes.includes(pathname)) {
+			return NextResponse.redirect(new URL('/', request.url))
 		}
+
+		if (!accessToken) {
+			try {
+				const res = await fetch(REFRESH_TOKEN_API_URL, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						Cookie: `refreshToken=${refreshToken}`
+					}
+				})
+
+				if (!res.ok) {
+					console.error('[Middleware] Refresh failed, logging out.')
+					const response = NextResponse.next()
+					response.cookies.delete('accessToken')
+					response.cookies.delete('refreshToken')
+					return response
+				}
+
+				const response = NextResponse.next()
+
+				const newCookies = (
+					res.headers as any
+				).getSetCookie() as string[]
+
+				if (newCookies && newCookies.length > 0) {
+					newCookies.forEach(cookie => {
+						response.headers.append('Set-Cookie', cookie)
+					})
+				}
+
+				return response
+			} catch (error) {
+				console.error(
+					'[Middleware] Network error during refresh:',
+					error
+				)
+				const response = NextResponse.next()
+				response.cookies.delete('accessToken')
+				response.cookies.delete('refreshToken')
+				return response
+			}
+		}
+
+		return NextResponse.next()
 	}
 
 	return NextResponse.next()
